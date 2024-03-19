2fc889277b53f7a278f8dd7d7977651a5c46953b
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2024-03-19 13:23:02 -0300

    clase 3

---
+++ clase-3/Notas.md
@@ -0,0 +1,14 @@
+## TypeScript
+
+superset de JavaScript, conjunto de normas que agregamos a JS, entre lo que mas destaca es el tipado (asignacion de que tipo de datos se permiten en distintas funciones, variables,etc.)
+
+
+# Para compilar
+Como los browsers no interpretan TypeScript, lo que hacemos es compilar de tsc a js por medio del comando en la terminal tsc ( que sacamos del paquete de typescript que instalamos en nuestra compu o en el proyecto segun querramos)
+
+
+## POO
+
+POO viene de la sigla Programacion Orientada a Objetos y es un paradigma de programacion(conjunto de reglas y principios de programación).
+
+Se enfoca en las entidades que participan en la resolucion de los probl
--- clase-3/Typescript/POO.ts
+++ clase-3/Typescript/POO.ts
@@ -1,8 +1,12 @@
 //recepcion de un consultorio de dentista
 //la POO se enfoca en las entidades encargadas de realizar esas acciones.
 
-//
-let listaDelDia:Array<any>=[]
+type PacienteInfo={
+    nombrePaciente:string,
+    edadPaciente:number,
+    nroLegajo:number
+}
+let listaDelDia:Array<PacienteInfo>=[]
 
 type Material={
     nombre:string
@@ -11,33 +15,127 @@ type Material={
 //molde
 class Recepcion{
     //atributos (caracteristicas)
-
+    private correo:string;
+    private nombre:string;
+    public diaLaborales:Array<string>
     //creo un constructor,para que apenas genere una instancia de la clase, la cree con informacion,con atributos
-     constructor(){
-    
+     constructor(correoR:string,nombreR:string,diasR:Array<any>){
+        this.correo=correoR;
+        this.nombre=nombreR;
+        this.diaLaborales=diasR
      }
 
     //metodos (comportamientos-capacidades)
-    cargarPaciente(paciente:any){
+    cargarPaciente(paciente:PacienteInfo){
         listaDelDia.push(paciente);
-        
+        console.log(listaDelDia+ " que se cargo dentro de los dias laborales del recepcionista que son :" + this.diaLaborales);
+
     };
 
-    saludar(){
-        alert("Te damos la bienvenida!! soy ")
+    public saludar(){
+        alert("Te damos la bienvenida!! soy "+ this.nombre)
     }
 
-    asignarTurno(){
+    private asignarTurno(){
         console.log("asigna turno al paciente ...")
+    }
+
+    armarAgenda(){
+        this.asignarTurno();
+        console.log('sacar la lista de pacientes de ese dia..')
+    }
+    //this hace referencia a la clase (en este caso Recepcion)
+}
+
+//Herencia 
+//superclase
+class ProfesionalBucal{
+    //atributos
+    private correo:string;
+    public matricula:number;
+    public nombre:string;
+
+    //asignar valores apenas creo una instancia
+    constructor(correo:string,matricula:number,nombre:string){
+        this.correo=correo;
+        this.matricula=matricula;
+        this.nombre=nombre;
+    }
+
+    //metodos
+    control(){
+        console.log(this.nombre +" chequea la boca del paciente..")
     }
 }
 
-//instancia de una clase 
+//subclase
+class Dentista extends ProfesionalBucal{
 
- const recepcionistaA= new Recepcion();
+    //metodo
+    tratamientoConducto(){
+        //console.log(this.correo +"Hace tratamiento conducto...")
+        //correo es atributo que heredo de ProfesionalBucal y me tira error porque es un atributo privado (solo lo puedo usar en la superclase ProfesionalBucal- donde se creo )
+        console.log(this.nombre +"Hace tratamiento conducto...")
+        //nombre es atributo que heredo de ProfesionalBucal ,y como es publico lo puedo tambien utilizar dentro de la subclase Dentista 
+    }
+}
+class Asistente extends ProfesionalBucal{
+
+        //atributos 
+        diasDisponible=["lun","mier","viernes"];
+        horarios:(number)[];
+
+        //generamos el contructor para que en la instancia de Asistente,indique el atributo de los horarios.
+        //le agregamos todos los parametros necesarios que trae del ProfesionalBucal,que le indicamos que asigne a los atributos que trae de esta otra clase con super()
+        constructor(horariosAsist:(number)[],correo:string,matricula:number,nombre:string){
+            super(correo,matricula,nombre)
+            this.horarios=horariosAsist;
+        }
+
+      
+        limpiezaGral(){
+            console.log("Hace limpieza gral de la boca del paciente...")
+        }
+}
+//instancia de una clase (con el new)
+
+ const recepcionistaA= new Recepcion("marta@recepcion.com","Marta Ruiz",["lunes","martes"]);
 
 
-//this
+ const recepcionB= new Recepcion("ruben@recepcion.com","Ruben Gomez",["Miercoles","Jueves"]);
+ //recepcionB.saludar();
+ //recepcionistaA.saludar();
 
+ const PacienteA:PacienteInfo={
+    nombrePaciente:"Juan Perez",
+    edadPaciente:33,
+    nroLegajo:221347
+ }
+
+recepcionistaA.cargarPaciente(PacienteA);
+recepcionB.cargarPaciente({
+    nombrePaciente:"Laura Lopez",
+    edadPaciente:19,
+    nroLegajo:22
+})
+
+const dentistaA= new Dentista("dentistaA@consultorio.com",11111,"Luis Luna");//Dentista subclase de ProfesionalBucal , el cual necesita que le pase 3 parametros para crearlo
+
+const asistenteA= new Asistente([9,10,11,12],"asistenteA",22222,"Pablo Piedra");
+
+dentistaA.control();
+asistenteA.control();
+dentistaA.tratamientoConducto();//solo el dentista, el asistente no tiene este metodo, esta capacidad
+asistenteA.limpiezaGral();
+
+console.log(asistenteA.nombre,asistenteA.horarios)
+
+recepcionistaA.saludar()//atributo "nombre" es privado, entonces solo lo puede utilizar dentro de la definicion de la clase , en la instancia no lo podemos usar.
+
+//recepcionistaA.correo me tira Typescript error por ser privado
+//recepcionB.asignarTurno()//tira error porque ese metodo se definio com privado(private),entonces solo se puede utilizar dentro de la clase (el molde)
+
+recepcionB.armarAgenda()//lo puedo hacer por que es publico y puedo utilizarlo por fuera de la clase, en la instancia
+console.log(recepcionistaA.diaLaborales)
 
 
