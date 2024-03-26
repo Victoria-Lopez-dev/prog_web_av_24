8724078b386a39c146100adaf230c6a8625e15be
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2024-03-26 10:27:05 -0300

    clase 5

---
+++ clase-5/notas.js
@@ -0,0 +1,50 @@
+/*
+C.R.U.D ? -> Create Reed Update Delete (acciones  a cargo de la API para modificar la info de la DB)
+
+DB -> base de datos| conjunto de informacion almacenada en un servidor, que nos brinda mas seguridad y estructura ,aumentando el costo del proyecto
+
+2 tipos de DB -> como almacena la informacion:
+//DB no relacionales 
+almacenan la informacion en colecciones ( grandes archivos de tipo JSON)
+mas descontracturadas
+capacidad mayor, mas libertad al momento de almacenar informacion
+necesito aplicarle mas estructura 
+
+JSON (JavaScript Object Notation)
+
+Ejemplo->MongoDB
+
+
+//DB relacionales
+
+almacenan la informacion en forma de tablas
+mas estructuradas
+filas ->registros ( informacion)
+columnas -> campos 
+
+Ejemplo-> MySQL,MariaDB 
+
+utilizan lenguaje SQL 
+
+
+podemos restringir en la tabla el tipo de dato permitido
+
+JS -> strings(caracteres) numbers boolean....
+
+SQL -> CHAR - VARCHAR (caracteres -265) | INTEGER (numeros enteros), DECIMAL| BOOl |TEXT
+
+XAMPP -> entorno de desarrollo ( programa para poder levantar una DB relacional )
+Workbench MySQL-> entorno de desarrollo ( programa para poder levantar una DB relacional )
+
+Nosotros vamos a usar XAMPP 
+
+puedo tener por cada DB relacional varias tablas 
+ */
+
+/* Despues del break:
+
+- SQL
+
+- Protocolo HTTP
+
+*/
